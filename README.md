457fefbba6539082ac16b438e5c0d13b7f73c299
Arun Nanduri <arun.nanduri@gmail.com>
2018-10-02 07:54:36 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bitcask-Lite
+
+This project aims to be a toy working version of a hash index for key-value data, the first storage engine described in _Designing Data-Intensive Applications_.
---
+++ bitcask.c
@@ -0,0 +1,101 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+//first, need to build repl functionality
+
+struct InputBuffer_t {
+  char* buffer;
+  size_t buffer_length;
+  ssize_t input_length;
+};
+typedef struct InputBuffer_t InputBuffer;
+
+InputBuffer* new_input_buffer() {
+  //allocate and initialize
+  InputBuffer* input_buffer = malloc(sizeof(InputBuffer));
+  input_buffer->buffer = NULL;
+  input_buffer->buffer_length = 0;
+  input_buffer->input_length = 0;
+  return input_buffer;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void read_input(InputBuffer* input_buffer) {
+  ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+  
+  if (bytes_read <= 0) {
+    printf("Error reading input\n");
+    exit(EXIT_FAILURE);
+  }
+  
+  //get rid of trailing newline
+  input_buffer->buffer[bytes_read - 1] = 0;
+  input_buffer->input_length = bytes_read - 1;
+}
+
+void print_prompt() {
+  printf("bitcask-lite>");
+}
+
+void process_command(InputBuffer* input_buffer) {
+  if (strcmp(input_buffer->buffer, ".exit") == 0) {
+    exit(EXIT_SUCCESS);
+  } else {
+    printf("Unrecognized command '%s'\n", input_buffer->buffer);
+  }
+}
+
+int main(int argc, char* argv[]) {
+  InputBuffer* input_buffer = new_input_buffer();
+  while (true) {
+    print_prompt();
+    read_input(input_buffer); //puts input in the buffer of input_buffer
+    
+    process_command(input_buffer);
+  }
+}
