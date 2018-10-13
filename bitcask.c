0dc1a84bf53b8208ba3b9b749f382c10872c723b
Arun Nanduri <arun.nanduri@gmail.com>
2018-10-13 12:43:26 -0400

    Begin adding code for in-memory hash table

--- bitcask.c
+++ bitcask.c
@@ -24,7 +24,73 @@ InputBuffer* new_input_buffer() {
   return input_buffer;
 }
 
+/* 
+ * Data layout of the segment file
+ */
+const uint32_t VALUE_SIZE = 255;
 
+const uint32_t VALUE_NUM_BYTES_SIZE = sizeof(VALUE_SIZE);
+
+struct Node_t {
+  //key (to match with when doing lookups)
+  int key;
+  //value stored as raw string
+  char value[VALUE_SIZE];
+  //pointer to next node
+  Node* next_node;
+};
+typedef struct Node_t Node;
+
+// hash function
+// a popular multiplier
+#define MULT 31
+unsigned int hash(char *p, int nhash) {
+  unsigned int h = 0;
+  for (; *p; p++) {
+    h = MULT * h + *p;
+  }
+  return h % nhash;
+}
+
+// returns pointer to array of pointers to linked lists
+// nhash denotes the length of the array of the hashmap
+Node** create_hashmap(int nhash) {
+  void* array = malloc(nhash * sizeof(Node*));
+  return (Node**)array;
+}
+
+Node* get_bucket(Node** hashmap, int key) {
+  unsigned int h = hash((char*)&key);
+  Node* bucket_node = hashmap[h];
+  while ((bucket_node->key != NULL) && (bucket_node->key != key)) {
+    bucket_node = bucket_node->next_node;
+  }
+  return bucket_node;
+}
+
+void insert_hashmap(Node** hashmap, int key, char* value) {
+  //hash the key
+  //get the linked list at that value in the hashmap array
+  //add a node with the value
+  Node* bucket_node = get_bucket(hashmap, key);
+  
+  bucket_node->key = key;
+  bucket_node->value = value;
+  bucket_node->next_node = (Node*)malloc(sizeof(Node*));
+  //is the key of the uninitialized but allocated next node
+  //set to NULL?
+}
+
+char* get_hashmap(Node** hashmap, int key) {
+  //hash the key
+  //get the linked list at that value in the hashmap array  
+  Node* bucket_node = get_bucket(hashmap, key);  
+  return bucket_node->value;
+}
+
+void cleanup_hashmap() {
+  //free all pointers
+}
 
 
 
@@ -66,7 +132,8 @@ InputBuffer* new_input_buffer() {
 
 
 void read_input(InputBuffer* input_buffer) {
-  ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+  ssize_t bytes_read = getline(&(input_buffer->buffer), 
+                               &(input_buffer->buffer_length), stdin);
   
   if (bytes_read <= 0) {
     printf("Error reading input\n");
